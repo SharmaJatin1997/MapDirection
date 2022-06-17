57c19004ecc3ec14c22270d9d14236799f49ecce
Jatin Sharma <jbhardwaj304@gmail.com>
2022-06-17 18:01:48 +0530

    Initial commit

---
+++ app/src/main/java/com/routes/googlemapsdirection/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.routes.googlemapsdirection
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.PolyUtil
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var googleMap: GoogleMap? = null
+    var googleURL: StringBuilder? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Initialize Google Maps and its callbacks
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+        val latLngOrigin = LatLng(10.3181466, 123.9029382) // Ayala
+        val latLngDestination = LatLng(10.311795, 123.915864) // SM City
+        this.googleMap!!.addMarker(MarkerOptions().position(latLngOrigin).title("Ayala"))
+        this.googleMap!!.addMarker(MarkerOptions().position(latLngDestination).title("SM City"))
+        this.googleMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngOrigin, 14.5f))
+        val path: MutableList<List<LatLng>> = ArrayList()
+
+        getUrl(latLngOrigin.latitude, latLngOrigin.longitude, latLngDestination.latitude, latLngDestination.longitude)
+
+        val urlDirections = googleURL.toString()
+        val directionsRequest = object :
+            StringRequest(Request.Method.GET, urlDirections, Response.Listener<String> { response ->
+                val jsonResponse = JSONObject(response)
+                // Get routes
+                val routes = jsonResponse.getJSONArray("routes")
+                val legs = routes.getJSONObject(0).getJSONArray("legs")
+                val steps = legs.getJSONObject(0).getJSONArray("steps")
+                for (i in 0 until steps.length()) {
+                    val points =
+                        steps.getJSONObject(i).getJSONObject("polyline").getString("points")
+                    path.add(PolyUtil.decode(points))
+                }
+                for (i in 0 until path.size) {
+                    this.googleMap!!.addPolyline(PolylineOptions().addAll(path[i]).color(Color.RED))
+                }
+            }, Response.ErrorListener { _ ->
+            }) {}
+        val requestQueue = Volley.newRequestQueue(this)
+        requestQueue.add(directionsRequest)
+    }
+
+
+    private fun getUrl(
+        latitide: Double,
+        longitude: Double,
+        deLatitide: Double,
+        deLongitude: Double
+    ): String {
+        googleURL = StringBuilder("https://maps.googleapis.com/maps/api/directions/json?")
+        googleURL!!.append("origin=$latitide,$longitude")
+        googleURL!!.append("&destination=$deLatitide,$deLongitude")
+        googleURL!!.append("&key=" + "Please enter map key")
+        return googleURL.toString()
+    }
+}
